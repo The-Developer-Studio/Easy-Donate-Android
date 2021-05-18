8d71b83b462e716fc6fa61646ed63d67452be60b
Madhavan S <gitmaddy.16@gmail.com>
2021-05-18 12:33:21 +0530

    Changed Structure and Added Documentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,149 +1,21 @@
+import 'package:easydonatefinal/splash.dart';
 import 'package:flutter/material.dart';
-import 'package:splashscreen/splashscreen.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    theme: new ThemeData(
-        primaryColor: Colors.deepOrange,
-        accentColor: Colors.deepOrangeAccent,
-        fontFamily: "Montserrat"),
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ));
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+// Root Widget
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new SplashScreen(
-        seconds: 3,
-        navigateAfterSeconds: new AfterSplash(),
-        image: Image.asset('images/logo.png'),
-        backgroundColor: Colors.white,
-        styleTextUnderTheLoader: new TextStyle(),
-        photoSize: 100.0,
-        loaderColor: Colors.deepOrangeAccent);
-  }
-}
-
-class AfterSplash extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              Center(
-                child: Image.asset(
-                  'images/logo.png',
-                  height: 100,
-                ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Text(
-                'Login to your Account',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                    fontFamily: "Montserrat",
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
-                height: 70,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    border: Border.all(color: Colors.black12)),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    labelText: "Email",
-                    labelStyle: TextStyle(fontSize: 14),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
-                height: 70,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    border: Border.all(color: Colors.black12)),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    labelText: "Password",
-                    labelStyle: TextStyle(fontSize: 14),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Center(
-                child: Container(
-                  width: 500,
-                  height: 50,
-                  // margin: EdgeInsets.all(25),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: FlatButton(
-                    child: Text(
-                      'Sign in',
-                      style: TextStyle(fontSize: 14.0),
-                    ),
-                    color: Colors.deepOrangeAccent,
-                    textColor: Colors.white,
-                    onPressed: () {},
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 60,
-              ),
-              Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Dont have an account? ',
-                      style: TextStyle(fontSize: 14.0),
-                    ),
-                    Text(
-                      'Sign up ',
-                      style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.deepOrange),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+    return new MaterialApp(
+      theme: new ThemeData(
+          primaryColor: Colors.deepOrange,
+          accentColor: Colors.deepOrangeAccent,
+          fontFamily: "Montserrat"),
+      debugShowCheckedModeBanner: false,
+      home: Splash(),
     );
   }
 }
