44aa7594c7bb7768b20152db23567727872e12d4
akill <akillika49@gmail.com>
2021-05-18 21:57:20 +0530

    Bottom nav bar and UI changes

---
+++ lib/icons.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyFlutterApp {
+  MyFlutterApp._();
+
+  static const _kFontFam = 'MyFlutterApp';
+  static const String _kFontPkg = null;
+
+  static const IconData icons8_handshake =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData icons8_gift =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData icons8_add_new =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData icons8_customer =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
