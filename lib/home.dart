02c4b965a8efec0ed3cf0deca944afbeb6612f84
akill <akillika49@gmail.com>
2021-05-18 21:04:29 +0530

    Basic Header UI

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
 import 'package:easydonatefinal/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,7 +12,130 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    // SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
     return Scaffold(
+      backgroundColor: Colors.white,
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 250.0,
+                floating: false,
+                pinned: true,
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25),
+                      child: Container(
+                        height: 35,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Center(
+                          child: TextField(
+                            style: TextStyle(fontSize: 10),
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Colors.green)),
+                                fillColor: Colors.white,
+                                hintText: 'Enter a search term',
+                                hintStyle: TextStyle(fontSize: 9)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    background: Padding(
+                      padding: const EdgeInsets.only(bottom: 25.0),
+                      child: Stack(
+                        children: [
+                          Container(
+                            // height: 100,
+                            decoration: BoxDecoration(
+                                color: Colors.deepOrange,
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(80),
+                                    bottomRight: Radius.circular(80))),
+                          ),
+                          Positioned(
+                            left: 45,
+                            top: 50,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Save a life',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                          fontSize: 20),
+                                    ),
+                                    Text(
+                                      'today!',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                          fontSize: 20),
+                                    ),
+                                    SizedBox(
+                                      height: 7,
+                                    ),
+                                    Text(
+                                      'find people who',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                          fontSize: 12),
+                                    ),
+                                    Text(
+                                      'needs a donation.',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                          fontSize: 12),
+                                    ),
+                                  ],
+                                ),
+                                Image.asset(
+                                  'images/header.png',
+                                  height: 130,
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+            ];
+          },
+          body: Center(),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           FirebaseAuth.instance
