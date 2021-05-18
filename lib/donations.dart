2fd2a9a5fc74d2cac8ac566c0453f448551b86e3
akill <akillika49@gmail.com>
2021-05-18 21:57:20 +0530

    Bottom nav bar and UI changes

---
+++ lib/donations.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DonationsPage extends StatefulWidget {
+  @override
+  _DonationsPageState createState() => _DonationsPageState();
+}
+
+class _DonationsPageState extends State<DonationsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Donations');
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,15 +1,20 @@
+import 'package:easydonatefinal/donations.dart';
 import 'package:easydonatefinal/login.dart';
+import 'package:easydonatefinal/requests.dart';
+import 'package:easydonatefinal/splash.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
+  const Home({Key key, this.destination}) : super(key: key);
+  final Destination destination;
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     // SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
@@ -18,123 +23,145 @@ class _HomeState extends State<Home> {
       body: DefaultTabController(
         length: 2,
         child: NestedScrollView(
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                expandedHeight: 250.0,
-                floating: false,
-                pinned: true,
-                elevation: 0,
-                backgroundColor: Colors.transparent,
-                flexibleSpace: FlexibleSpaceBar(
-                    centerTitle: true,
-                    title: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 25),
-                      child: Container(
-                        height: 35,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Center(
-                          child: TextField(
-                            style: TextStyle(fontSize: 10),
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.green)),
-                                fillColor: Colors.white,
-                                hintText: 'Enter a search term',
-                                hintStyle: TextStyle(fontSize: 9)),
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  expandedHeight: 250.0,
+                  floating: false,
+                  pinned: true,
+                  elevation: 0,
+                  backgroundColor: Colors.transparent,
+                  flexibleSpace: FlexibleSpaceBar(
+                      centerTitle: true,
+                      title: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        child: Container(
+                          height: 35,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black38,
+                                blurRadius: 2,
+                                offset: const Offset(0, 1),
+                              ),
+                            ],
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5),
                           ),
-                        ),
-                      ),
-                    ),
-                    background: Padding(
-                      padding: const EdgeInsets.only(bottom: 25.0),
-                      child: Stack(
-                        children: [
-                          Container(
-                            // height: 100,
-                            decoration: BoxDecoration(
-                                color: Colors.deepOrange,
-                                borderRadius: BorderRadius.only(
-                                    bottomLeft: Radius.circular(80),
-                                    bottomRight: Radius.circular(80))),
-                          ),
-                          Positioned(
-                            left: 45,
-                            top: 50,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      'Save a life',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                          fontSize: 20),
-                                    ),
-                                    Text(
-                                      'today!',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                          fontSize: 20),
-                                    ),
-                                    SizedBox(
-                                      height: 7,
-                                    ),
-                                    Text(
-                                      'find people who',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                          fontSize: 12),
-                                    ),
-                                    Text(
-                                      'needs a donation.',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                          fontSize: 12),
-                                    ),
-                                  ],
-                                ),
-                                Image.asset(
-                                  'images/header.png',
-                                  height: 130,
-                                )
-                              ],
+                          child: Center(
+                            child: TextField(
+                              style: TextStyle(fontSize: 10),
+                              decoration: InputDecoration(
+                                  suffixIcon: Icon(
+                                    Icons.search,
+                                    color: Colors.black12,
+                                    size: 20,
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.white70,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(12.0)),
+                                    borderSide: BorderSide(
+                                        color: Colors.transparent, width: 2),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10.0)),
+                                    borderSide: BorderSide(
+                                        color: Colors.transparent, width: 2),
+                                  ),
+                                  hintText: 'Enter a search term',
+                                  hintStyle: TextStyle(fontSize: 9)),
                             ),
                           ),
-                        ],
+                        ),
                       ),
-                    )),
-              ),
-            ];
-          },
-          body: Center(),
-        ),
+                      background: Padding(
+                        padding: const EdgeInsets.only(bottom: 25.0),
+                        child: Stack(
+                          children: [
+                            Container(
+                              // height: 100,
+                              decoration: BoxDecoration(
+                                  color: Colors.deepOrange,
+                                  borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(80),
+                                      bottomRight: Radius.circular(80))),
+                            ),
+                            Positioned(
+                              left: 45,
+                              top: 50,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'Save a life',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                            fontSize: 20),
+                                      ),
+                                      Text(
+                                        'today!',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                            fontSize: 20),
+                                      ),
+                                      SizedBox(
+                                        height: 7,
+                                      ),
+                                      Text(
+                                        'find people who',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                            fontSize: 12),
+                                      ),
+                                      Text(
+                                        'needs a donation.',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                            fontSize: 12),
+                                      ),
+                                    ],
+                                  ),
+                                  Image.asset(
+                                    'images/header.png',
+                                    height: 130,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      )),
+                ),
+              ];
+            },
+            body: Center()),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: allDestinations.map((Destination destination) {
+          return BottomNavigationBarItem(
+              icon: Icon(destination.icon),
+              backgroundColor: destination.color,
+              title: Text(destination.title));
+        }).toList(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -152,3 +179,17 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
+class Destination {
+  const Destination(this.title, this.icon, this.color);
+  final String title;
+  final IconData icon;
+  final MaterialColor color;
+}
+
+const List<Destination> allDestinations = <Destination>[
+  Destination('Requests', Icons.access_time_outlined, Colors.deepOrange),
+  Destination('Donations', Icons.card_giftcard, Colors.orange),
+  Destination('Post new', Icons.add_circle_outline_rounded, Colors.deepOrange),
+  Destination('Account', Icons.account_circle_outlined, Colors.orange)
+];
