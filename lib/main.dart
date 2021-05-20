4df471aa2765decd96c42f2cf27d996af5da0776
Madhavan S <gitmaddy.16@gmail.com>
2021-05-20 22:25:28 +0530

    File Structure Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:easydonatefinal/account.dart';
 import 'package:easydonatefinal/donations.dart';
-import 'package:easydonatefinal/home.dart';
 import 'package:easydonatefinal/post.dart';
 import 'package:easydonatefinal/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -30,56 +29,6 @@ class MyApp extends StatelessWidget {
           fontFamily: "Montserrat"),
       debugShowCheckedModeBanner: false,
       home: Splash(),
-    );
-  }
-}
-
-class BottomNavigation extends StatefulWidget {
-  @override
-  _BottomNavigationState createState() => _BottomNavigationState();
-}
-
-class _BottomNavigationState extends State<BottomNavigation> {
-  int _currentIndex = 0;
-  final List<Widget> _children = [
-    Home(),
-    DonationsPage(),
-    PostNew(),
-    AccountPage(),
-  ];
-
-  void onTappedBar(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTappedBar,
-        currentIndex: _currentIndex,
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: new Text('Requests'),
-              backgroundColor: Colors.deepOrange),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.card_giftcard),
-              title: new Text('Donations'),
-              backgroundColor: Colors.orange),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.add_box_outlined),
-              title: new Text('Post new'),
-              backgroundColor: Colors.deepOrange),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle_outlined),
-              title: new Text('Accounts'),
-              backgroundColor: Colors.orange),
-        ],
-      ),
     );
   }
 }
