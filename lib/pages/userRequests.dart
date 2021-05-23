cbc79658a5fbecd85b2f8c0f293b2a089ae51c9d
akill <akillika49@gmail.com>
2021-05-23 20:08:50 +0530

    Delete and edit button

--- lib/pages/userRequests.dart
+++ lib/pages/userRequests.dart
@@ -8,25 +8,133 @@ class UserRequests extends StatefulWidget {
 }
 
 class _UserRequestsState extends State<UserRequests> {
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text(
+            'Delete post',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text(
+                  'This action will delete the post permenantly, you cant restore the post after deletion.',
+                  style: TextStyle(fontSize: 14),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'Cancel',
+                style: TextStyle(
+                    color: Colors.black45, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text(
+                'Delete',
+                style:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.deepOrange,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
       body: StreamBuilder(
-        stream: getUserRequests(),
+        stream: getRequests(),
         builder: (context, snapshot) {
           return snapshot.hasData
               ? ListView.builder(
                   shrinkWrap: true,
                   itemCount: snapshot.data.size,
                   itemBuilder: (context, index) {
-                    return ListCard(
-                      location: snapshot.data.docs[index]['location'],
-                      postedTime:
-                          duration(snapshot.data.docs[index]['postedTime']),
-                      title: snapshot.data.docs[index]['title'],
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          child: Text(
+                            'Request history',
+                            style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black87),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          child: Text(
+                            'Please review your details before posting. Anyways you can edit the post even after posting!',
+                            style: TextStyle(
+                                fontSize: 12,
+                                // fontWeight: FontWeight.bold,
+                                color: Colors.black45),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                child: ListCard(
+                                  location: snapshot.data.docs[index]
+                                      ['location'],
+                                  postedTime: duration(
+                                      snapshot.data.docs[index]['postedTime']),
+                                  title: snapshot.data.docs[index]['title'],
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                _showMyDialog();
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.only(right: 20),
+                                child: Icon(
+                                  Icons.delete,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(right: 20),
+                              child: Icon(
+                                Icons.edit,
+                                color: Colors.deepPurple,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     );
                   })
               : Center(
