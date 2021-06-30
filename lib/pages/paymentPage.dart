b0be4af0a37800eba6cf8ebff5ab028298ba9b67
akill <akillika49@gmail.com>
2021-06-30 05:47:47 +0530

    Razorpay added

---
+++ lib/pages/paymentPage.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+
+class PaymentPage extends StatefulWidget {
+  @override
+  _PaymentPageState createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  Razorpay razorpay;
+  TextEditingController textEditingController = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    razorpay = new Razorpay();
+    razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
+    razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerErrorFailure);
+    razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExternalWallet);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    razorpay.clear();
+  }
+
+  void openCheckout() {
+    var options = {
+      "key": "rzp_test_mA207SOSIPg03y",
+      "amount": num.parse(textEditingController.text) * 100,
+      "name": "The Developer Studio",
+      "description": "Donation for the developers for Easy Donate",
+      // "prefill": {}
+      "external": {
+        "wallets": ["paytm"]
+      }
+    };
+    try {
+      razorpay.open(options);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void handlerPaymentSuccess() {
+    print("Payment Success");
+  }
+
+  void handlerErrorFailure() {
+    print("Payment Failure");
+  }
+
+  void handlerExternalWallet() {
+    print("Payment External wallet");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20.0, left: 25, right: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Enter the amount to donate',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              controller: textEditingController,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: openCheckout,
+              child: Container(
+                color: Colors.deepOrangeAccent,
+                // height: 30,
+                // width: 200,
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50.0, vertical: 15),
+                    child: Text(
+                      "Donate now",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
